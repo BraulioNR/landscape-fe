3011185283fdf6fc483b91a29d48042ebdce33b5
BraulioNR <branoleruiz@gmail.com>
2022-05-24 02:05:20 -0500

    Change url_backend

--- src/environments/environments.js
+++ src/environments/environments.js
@@ -1,2 +1,2 @@
-export const URL_BACKEND = "http://localhost:8000"
+export const URL_BACKEND = "https://landscape-be.herokuapp.com/"
 export const URL_MAPBOX = "https://api.mapbox.com/geocoding/v5/mapbox.places"
