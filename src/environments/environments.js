ddee89cae87531ee27149c8f9a6c0b1aa1ed9407
BraulioNR <53287676+BraulioNR@users.noreply.github.com>
2023-04-17 01:38:13 -0500

    Update url railway

--- src/environments/environments.js
+++ src/environments/environments.js
@@ -1,2 +1,2 @@
 export const URL_MAPBOX = "https://api.mapbox.com/geocoding/v5/mapbox.places"
-export const URL_BACKEND = "https://landscape-be-production.up.railway.app/"
+export const URL_BACKEND = "https://landscape-be-production.up.railway.app"
